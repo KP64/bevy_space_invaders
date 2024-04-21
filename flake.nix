1dea293e7897cbfd8cc0226f3663857553fecd5b
KP64 <karamalsadeh@hotmail.com>
2024-04-21 12:08:31 +0000

    Flake Utils

--- flake.nix
+++ flake.nix
@@ -3,32 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
-      devShells.${system}.default = pkgs.mkShell rec {
-        nativeBuildInputs = with pkgs; [
-          nodejs
-          pkg-config
-          rust-analyzer
-        ];
-        buildInputs = with pkgs; [
-          openssl
-          udev alsa-lib vulkan-loader
-          xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
-          libxkbcommon wayland
-          
-          rustup
-          mold
-          llvmPackages_18.libcxxClang
-        ];
-        LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            nodejs
+            pkg-config
+            rust-analyzer
+          ];
+          buildInputs = with pkgs; [
+            openssl
+            udev
+            alsa-lib
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            libxkbcommon
+            wayland
+
+            rustup
+            mold
+            llvmPackages_18.libcxxClang
+          ];
+          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
 }
