4be943e4b5e19eb0d8cc4904686ace43ad663a6b
KP64 <karamalsadeh@hotmail.com>
2024-04-22 21:51:23 +0000

    nil Linter

--- flake.nix
+++ flake.nix
@@ -8,11 +8,11 @@
 
   outputs =
     {
-      self,
+      # self,
       nixpkgs,
       flake-utils,
       ...
-    }@inputs:
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
